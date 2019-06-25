dc865570c72aadfb28f0e6d8a55df0032a0b9fbd
Ankita Jain <36221512+Ankita1223@users.noreply.github.com>
2019-06-25 06:44:51 +0530

    Add files via upload

---
+++ database/constants.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+define("HOST","localhost");
+define("USER","root");
+define("PASS","");
+define("DB","inventory_management_system");
+
+define("DOMAIN","http://localhost/inventory_management_system");
+
+?>
